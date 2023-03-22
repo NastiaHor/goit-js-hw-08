6f4e065e671a1922f4659bf7e72dad78f7564c01
Anastasia Horbenko <anastasiakopylova00@gmail.com>
2023-03-23 00:50:05 +0200

    Create Task 2 - video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
-
+import vimeoPlayer from '@vimeo/player'; 
+import throttle from 'lodash.throttle'; 
+ 
+const localStorageKey = 'videoplayer-current-time'; 
+const player = new vimeoPlayer('vimeo-player'); 
+const storedTime = localStorage.getItem(localStorageKey); 
+ 
+if (storedTime) { 
+  player.setCurrentTime(storedTime); 
+} 
+ 
+function saveCurrentTimeToLocalStorage(data) { 
+  localStorage.setItem(localStorageKey, JSON.stringify(data.seconds)); 
+} 
+ 
+player.on('timeupdate', throttle(saveCurrentTimeToLocalStorage, 1000));
